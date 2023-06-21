5409c078fa28d090ef003af6f848812768815b4a
piyushjain5350 <126599802+piyushjain5350@users.noreply.github.com>
2023-06-21 14:57:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function getFormvalue() {
     //Write your code here
+	const fName=document.getElementsByTagName("input")[0].value;
+	const lName=document.getElementsByTagName("input")[1].value;
 
+	// const res=inputTag;
+	// console.log(res);
+	alert(fName+" "+lName);
 }
